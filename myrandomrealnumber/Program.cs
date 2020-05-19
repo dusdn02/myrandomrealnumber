4443ba93341b9c153441e588b2eab8512b9a0835
연우 이 <lyw9114@gmail.com>
2020-05-19 11:46:19 +0900

    초기 커밋.

---
+++ myrandomrealnumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myrandomrealnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+           int ran= r.Next();
+            while (true)
+            {
+                Console.WriteLine("숫자를 입력해주세요 : ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num > ran)
+                {
+                    Console.WriteLine(num + "보다는 작은 숫자 입니다.");
+                }else if (num < ran)
+                {
+                    Console.WriteLine(num + "보다는 큰 숫자 입니다.");
+                }
+                else
+                {
+                    Console.WriteLine("정답입니다!");
+                    break;
+                }
+            }
+        }
+    }
+}
